7d2654030565619ff6fa4d1fd3c72222f508e1a3
vividsystem <notrealbutnotobot@gmail.com>
2025-09-27 14:54:52 +0200

    feat: added .env config

---
+++ src/__main__.py
@@ -0,0 +1,4 @@
+from cfg import Settings
+
+if __name__=="__main__":
+    s = Settings()
---
+++ src/cfg/__init__.py
@@ -0,0 +1,32 @@
+from pydantic import Field, model_validator
+from pydantic_settings import BaseSettings, SettingsConfigDict
+from typing import Optional, Literal
+
+class UsbSettings(BaseSettings):
+    vendor_id: str 
+    product_id: str 
+
+class NetworkSettings(BaseSettings):
+    ip: str
+
+
+class Settings(BaseSettings):
+    model_config = SettingsConfigDict(
+        env_prefix="PRINTER__", 
+        env_file='.env', 
+        env_file_encoding='utf-8',
+        env_nested_delimiter="__"
+    )
+    profile: Optional[str] = None
+    connection: Literal["USB", "NETWORK"] = Field("USB")
+    network: Optional[NetworkSettings] = None
+    usb: Optional[UsbSettings] = None
+
+    @model_validator(mode='after')
+    def check_connections(cls, values):
+        if values.connection == "USB" and values.usb:
+            return values
+        if values.connection == "NETWORK" and values.network:
+            return values
+        raise ValueError("you have to specify your connection type")
+
