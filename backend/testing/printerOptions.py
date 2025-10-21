c1a4e5506868aefe2bb3551c6f9eda3a3454a997
vividsystem <notrealbutnotobot@gmail.com>
2025-10-21 23:59:06 +0200

    fix: printer options testing range func

--- backend/testing/printerOptions.py
+++ backend/testing/printerOptions.py
@@ -8,27 +8,11 @@ p = Printer(s)
 
 
 if __name__ == "__main__":
-    p.driver.block_text(
-        "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla laoreet risus ut varius hendrerit. Proin cursus mollis augue et vestibulum. Ut consequat dictum lacus facilisis tincidunt. Vestibulum nibh velit, semper vel elit semper, porta auctor purus. Vivamus aliquam risus posuere massa fermentum auctor at a neque. Pellentesque iaculis erat est, vel scelerisque elit convallis vestibulum. Pellentesque dignissim sodales nisl, et tincidunt odio faucibus vitae."
-    )
-
-    p.driver.ln()
-
-    p.driver.text(
-        "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla laoreet risus ut varius hendrerit. Proin cursus mollis augue et vestibulum. Ut consequat dictum lacus facilisis tincidunt. Vestibulum nibh velit, semper vel elit semper, porta auctor purus. Vivamus aliquam risus posuere massa fermentum auctor at a neque. Pellentesque iaculis erat est, vel scelerisque elit convallis vestibulum. Pellentesque dignissim sodales nisl, et tincidunt odio faucibus vitae."
-    )
-
-    p.driver.ln()
-
-    p.driver.qr("https://github.com/vividsystem/pos-todo")
-
-    p.driver.ln()
-
     TextOptions("right").set(p.driver)
     p.driver.textln("right aligned text")
     p._reset()
 
-    for i in range(0, 2):
+    for i in range(3):
         TextOptions(underlineType=i).set(p.driver)
         p.driver.text(f"type {i}")
 
@@ -42,6 +26,6 @@ if __name__ == "__main__":
     p._reset()
     p.driver.ln()
 
-    for i in range(0, 8):
+    for i in range(9):
         TextOptions(density=i).set(p.driver)
         p.driver.text(f"density {i}")
