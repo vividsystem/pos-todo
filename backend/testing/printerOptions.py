a34ecf3f734c35d10d35e6abb192100ff11f264d
vividsystem <notrealbutnotobot@gmail.com>
2025-10-22 00:06:17 +0200

    chore: adding a few more things to test

--- backend/testing/printerOptions.py
+++ backend/testing/printerOptions.py
@@ -14,18 +14,32 @@ if __name__ == "__main__":
 
     for i in range(3):
         TextOptions(underlineType=i).set(p.driver)
-        p.driver.text(f"type {i}")
+        p.driver.textln(f"type {i}")
 
     p.driver.ln()
     p._reset()
 
     for i in ["a", "b"]:
         TextOptions(font=i).set(p.driver)
-        p.driver.text(f"type {i}")
+        p.driver.textln(f"type {i}")
 
     p._reset()
     p.driver.ln()
 
     for i in range(9):
         TextOptions(density=i).set(p.driver)
-        p.driver.text(f"density {i}")
+        p.driver.textln(f"density {i}")
+
+    p._reset()
+    for i in range(1, 9):
+        for j in range(1, 9):
+            TextOptions(width=i, height=j)
+            p.driver.textln(f"this text is printed with h={j} w={i}")
+
+    p._reset()
+
+
+# Findings
+# when using .text() text doesnt wrap lines!
+# -> always use textln
+# font type b looks shitty
