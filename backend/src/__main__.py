6428db16b98ef331eb3ec9726bc9b5059c1dc5bc
vividsystem <notrealbutnotobot@gmail.com>
2025-10-22 13:18:32 +0200

    fix: added initializing function for mqtt

--- backend/src/__main__.py
+++ backend/src/__main__.py
@@ -1,5 +1,5 @@
 from cfg import Settings
-from mqtt import client
+from mqtt import client, initialize
 from printer import Printer
 import logging
 
@@ -10,5 +10,6 @@ if __name__ == "__main__":
 
     client.enable_logger()
     client.user_data_set(Printer(s))
+    initialize()
     client.connect(s.mqtt.host, s.mqtt.port)
     client.loop_forever()
