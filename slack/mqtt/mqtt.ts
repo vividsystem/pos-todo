0562a108974697680386224a893a9ceb98d9c735
vividsystem <notrealbutnotobot@gmail.com>
2025-10-27 15:24:58 +0100

    style: added space between id and username for better line-breaks

--- slack/mqtt/mqtt.ts
+++ slack/mqtt/mqtt.ts
@@ -6,7 +6,7 @@ const client = mqtt.connect(process.env.MQTT_HOST!)
 export async function printMessage(text: string, header: { name: string, id: string }) {
 	client.publishAsync("pos-todo/print/message", JSON.stringify({
 		message: text,
-		header: `Message from ${header.name}(${header.id}):`,
+		header: `Message from ${header.name} (${header.id}):`,
 		footer: `printed using pos-todo slack`
 	}))
 }
